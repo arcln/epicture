a1db52c686be542bf9cc34b018376f04ef7b8515
Paul Laffitte <paul.laffitte@epitech.eu>
2018-11-06 22:46:41 +0100

    auth and generic routing

--- api/Imgur.js
+++ api/Imgur.js
@@ -4,11 +4,33 @@ export default class {
     constructor(clientId, clientSecret, errorHandler) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
-        this.errorHandler = errorHandler || console.error;
+        this.errorHandler = errorHandler || (e => console.error(JSON.stringify(e.response.data)));
         this.axios = axios.create({
             baseURL: 'https://api.imgur.com/3',
             timeout: 1000,
             headers: {'Authorization': `Client-ID ${this.clientId}`}
+        });
+
+        this.routes = {
+            gallery: [
+                {name: 'section',   type: '/'},
+                {name: 'sort',      type: '/'},
+                {name: 'window',    type: '/'},
+                {name: 'page',      type: '/'},
+                {name: 'showViral', type: '&'},
+                {name: 'mature',    type: '&'},
+                {name: 'album_previews',  type: '&'},
+            ],
+        };
+
+        Object.keys(this.routes).map(key => {
+            this[key] = async (opts={}) => {
+                let args = this.routes[key];
+                let query = this.buildQuery(key, opts, args);
+                console.log('query:', query);
+                return this.axios.get(query)
+                    .catch(this.errorHandler);
+            };
         });
     }
 
@@ -30,22 +52,5 @@ export default class {
                     throw new Error(`invalid arg type "${arg.type}"`);
             }
         }).join('');
-    }
-
-    async gallery(opts={}) {
-        let args = [
-            {name: 'section',   type: '/'},
-            {name: 'sort',      type: '/'},
-            {name: 'window',    type: '/'},
-            {name: 'page',      type: '/'},
-            {name: 'showViral', type: '&'},
-            {name: 'mature',    type: '&'},
-            {name: 'album_previews',  type: '&'},
-        ];
-
-        let query = this.buildQuery('gallery', opts, args);
-        console.log('query:', query);
-        return this.axios.get(query)
-            .catch(this.errorHandler);
     }
 }
