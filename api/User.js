9b4974c8b3222975ad3e0ce13833ceadf2e99ec5
Paul Laffitte <paul.laffitte@epitech.eu>
2018-11-15 21:55:59 +0100

    user cache

--- api/User.js
+++ api/User.js
@@ -1,16 +1,39 @@
 import {AsyncStorage} from 'react-native';
 
-export default class {
+class User {
+
+    static lastUpdate = 0;
+    static cache = {};
+    static subscribers = [];
+    static triggerChange = () => User.subscribers.forEach(f => f(User.cache));
 
     static async get() {
-        return JSON.parse(await AsyncStorage.getItem('user'));
+        let now = Date.now();
+
+        if (now - User.lastUpdate > 60 * 60 * 1000) {
+            User.cache = JSON.parse(await AsyncStorage.getItem('user'))
+            User.lastUpdate = now;
+            console.log('put user in memory cache');
+        }
+
+        return User.cache;
     }
 
     static async set(user) {
-        await AsyncStorage.setItem('user', JSON.stringify(user));
+        User.cache = JSON.stringify(user);
+        await AsyncStorage.setItem('user', User.cache);
+        User.triggerChange();
     }
 
     static async logout() {
+        User.cache = null;
         await AsyncStorage.removeItem('user');
+        User.triggerChange();
+    }
+
+    static onChange(callback) {
+        User.subscribers.push(callback);
     }
 }
+
+export default User;
