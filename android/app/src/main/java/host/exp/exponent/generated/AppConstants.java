a785cf413aaac96842df3154e5fa96dbcb809ea2
Arthur Chaloin <arthur@achaloin.com>
2018-11-18 19:07:46 +0100

    add android manual build files for delivery

---
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -0,0 +1,51 @@
+package host.exp.exponent.generated;
+
+import com.facebook.common.internal.DoNotStrip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import host.exp.exponent.BuildConfig;
+import host.exp.exponent.Constants;
+
+@DoNotStrip
+public class AppConstants {
+
+  public static final String VERSION_NAME = "2.9.2";
+  public static String INITIAL_URL = "exp://exp.host/@arthurcln/epicture";
+  public static final boolean IS_DETACHED = true;
+  public static final String SHELL_APP_SCHEME = "exp59477e0054e14ae8b69bc4cb8edc6099";
+  public static final String RELEASE_CHANNEL = "default";
+  public static boolean SHOW_LOADING_VIEW_IN_SHELL_APP = false;
+  public static boolean ARE_REMOTE_UPDATES_ENABLED = true;
+  public static final List<Constants.EmbeddedResponse> EMBEDDED_RESPONSES;
+  public static boolean FCM_ENABLED = false;
+
+  static {
+    List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
+
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@arthurcln/epicture", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40arthurcln%2Fepicture%2F1.0.0%2F486e2254b9fbdbb41808e1fd646361dd-31.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
+    EMBEDDED_RESPONSES = embeddedResponses;
+  }
+
+  // Called from expoview/Constants
+  public static Constants.ExpoViewAppConstants get() {
+    Constants.ExpoViewAppConstants constants = new Constants.ExpoViewAppConstants();
+    constants.VERSION_NAME = VERSION_NAME;
+    constants.INITIAL_URL = INITIAL_URL;
+    constants.IS_DETACHED = IS_DETACHED;
+    constants.SHELL_APP_SCHEME = SHELL_APP_SCHEME;
+    constants.RELEASE_CHANNEL = RELEASE_CHANNEL;
+    constants.SHOW_LOADING_VIEW_IN_SHELL_APP = SHOW_LOADING_VIEW_IN_SHELL_APP;
+    constants.ARE_REMOTE_UPDATES_ENABLED = ARE_REMOTE_UPDATES_ENABLED;
+    constants.EMBEDDED_RESPONSES = EMBEDDED_RESPONSES;
+    constants.ANDROID_VERSION_CODE = BuildConfig.VERSION_CODE;
+    constants.FCM_ENABLED = FCM_ENABLED;
+    return constants;
+  }
+}
