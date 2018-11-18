7517d2bbe605ba546c9a793112b981770887dc66
Arthur Chaloin <arthur@achaloin.com>
2018-11-18 14:43:45 +0100

    splash screen and landing animation

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -5,6 +5,9 @@ import {
   Button,
   Text,
   StatusBar,
+  Dimensions,
+  Animated,
+  Easing,
 } from 'react-native';
 import {AuthSession, LinearGradient} from 'expo';
 import User from '../api/User';
@@ -18,13 +21,67 @@ export default class LandingScreen extends React.Component {
 
   state = {
     user: {},
+    landingOpacity: new Animated.Value(0.0),
+    splashOpacity: new Animated.Value(1.0),
+    btnOpacity: new Animated.Value(0.0),
+    textOpacity: new Animated.Value(0.0),
+    logoPos: new Animated.Value(0.0),
+    done: false,
   }
 
   async componentDidMount() {
+    await this.setState({
+      btnOpacity: new Animated.Value(0.0),
+    });
     await this.setState({user: await User.get()});
+
+    let {
+      splashOpacity,
+      landingOpacity,
+      btnOpacity,
+      textOpacity,
+      logoPos,
+    } = this.state;
+
+    const animation = Animated.sequence([
+      Animated.parallel([
+        Animated.timing(landingOpacity, {
+          toValue: 1.0,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(splashOpacity, {
+          toValue: 0.0,
+          duration: 1000,
+        }),
+      ]),
+      Animated.parallel([
+        Animated.timing(logoPos, {
+          toValue: -100,
+          duration: 500,
+          useNativeDriver: true,
+          easing: Easing.out(Easing.cubic),
+        }),
+        Animated.timing(textOpacity, {
+          toValue: 1.0,
+          duration: 500,
+          delay: 400,
+          useNativeDriver: true,
+        }),
+        Animated.timing(btnOpacity, {
+          toValue: 1.0,
+          duration: 500,
+          delay: 600,
+          useNativeDriver: true,
+        }),
+      ]),
+    ]);
+
+    setTimeout(() => animation.start(() => this.setState({done: true})), 500);
   }
 
   async login() {
+    console.log('ok');
     let user = this.state.user;
 
     if (!user) {
@@ -44,26 +101,70 @@ export default class LandingScreen extends React.Component {
     return (
       <View style={{flex: 1}}>
         <StatusBar barStyle='light-content' />
-        <LinearGradient
-          colors={['#2e2e82', '#171544']}
-          style={styles.container}>
-          <View style={styles.greeting}>
-            <Text style={{color: '#fff'}}>Welcome to</Text>
-            <AutoImage
-              source={require('../assets/images/logo.png')}
-              width={300}
-            />
-          </View>
-          <View>
+        <Animated.View style={{opacity: this.state.landingOpacity}}>
+          <LinearGradient
+            colors={['#2e2e82', '#171544']}
+            style={{
+              position: 'absolute',
+              height: Dimensions.get('window').height,
+              width: Dimensions.get('window').width,
+            }}
+          />
+        </Animated.View>
+        <View style={styles.container}>
+          <Animated.View style={{opacity: this.state.landingOpacity, ...styles.container, flex: 2}}>
+            <Animated.View style={[styles.greeting, {
+              position: 'absolute', top: Dimensions.get('window').height / 2 - 68, transform: [
+                {translateY: this.state.logoPos}
+              ]
+            }]}>
+              <AutoImage
+                source={require('../assets/images/splash.png')}
+                width={Dimensions.get('window').width}
+              />
+            </Animated.View>
+          </Animated.View>
+          <Animated.View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            opacity: this.state.btnOpacity,
+            paddingBottom: Dimensions.get('window').height / 4,
+          }}>
             <Button
               onPress={() => this.login()}
               title="Login"
               color="#1bb76e"
+              // disabled={this.state.btnOpacity == 0.0}
               style={{flex: 5}}
             />
+          </Animated.View>
+        </View>
+        <Animated.View style={{
+          position: 'absolute',
+          top: Dimensions.get('window').height / 4 * 2 - 30,
+          width: Dimensions.get('window').width,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          opacity: this.state.textOpacity,
+        }}>
+          <Text>The magic of the internet</Text>
+        </Animated.View>
+        <Animated.View style={{
+          opacity: this.state.splashOpacity,
+          position: 'absolute',
+          top: Dimensions.get('window').height / 2 - 68,
+          transform: [{translateY: this.state.done ? -1000 : 0}],
+        }}>
+          <View style={styles.container}>
+            <View style={styles.greeting}>
+              <AutoImage
+                source={require('../assets/images/splash.png')}
+                width={Dimensions.get('window').width}
+              />
+            </View>
           </View>
-        </LinearGradient>
-      </View>
+        </Animated.View>
+      </View >
     );
   }
 };
@@ -72,12 +173,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'space-around',
+    justifyContent: 'space-around',
   },
   greeting: {
     alignItems: 'center',
     height: 300,
-    marginTop: 90,
   }
 });
